78f67e3aa9ce4846ecc934a89c6f3e491ff77e63
dirtbag <wheatus@hanmail.net>
2016-12-27 02:36:22 +0900

    new boggro
    new boggro

--- scripts/app.js
+++ scripts/app.js
@@ -1,56 +1,118 @@
 (function(){
-	var app = angular.module('Boggro', ['ngAudio']);
-	
-	app.controller('MainCtrl', ['$scope', '$http', 'ngAudio', function ($scope, $http, ngAudio){
-		var main = this,
-			befAudioId = null;
-		
-		$scope.sounds = [];
-		$scope.isLoaded = [];
-		$scope.col = [];
+	var app = angular.module('Boggro', ['ui.router', 'ngTagCloud', 'duScroll']);
 
-		//ngAudio
-		$scope.bell = ngAudio.load("sounds/Jingle Bells in Kazoo.mp3");
-		$scope.bell.loop = true;
+	app.config(
+		['$stateProvider', '$locationProvider',
+			function($stateProvider, $locationProvider){
+				$locationProvider.html5Mode({
+					enabled: true,
+					requireBase: false
+				});
+				$stateProvider
+					.state('/', {
+						url:'/:tags',
+						views: {
+							'content': {
+								templateUrl: 'view/content.html',
+								controller: 'contentCtrl',
+								controllerAs: 'content'
+							}
+						}
+					})
+			}
+		]
+	);
 
-		(function(){
-			$http.get('sounds.json').success(function (data){
-				$scope.sounds = data;
-				data.forEach(function(item,index){
-					$scope.isLoaded.push(false);
-					if(index==0){
-						$scope.col.push(true);
-					}else{
-						$scope.col.push(false);
+	function checkAccessOnStateChange($rootScope){
+		$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+			if(toParams.tags !== ""){
+				var elem = angular.element('#tag');
+				if(elem !== null){
+					angular.element(document).scrollToElementAnimated(elem);
+				}
+			}
+		});
+	}
+
+	checkAccessOnStateChange.$inject = ['$rootScope'];
+	app.run(checkAccessOnStateChange);
+// main controller
+	function main($scope, $http, $state, $stateParams, $location, $anchorScroll){
+		var tagArray = [];
+
+		$scope.data = [];
+
+		$http.get('sounds.json').then(function (res){
+			res.data.files.forEach(function(item,index){//files
+				item.tag.forEach(function(item2,index2){//files.tag
+					//var a = search(item2,tagArray);
+					var a = _.find(tagArray, function(tags){
+						return tags.text == item2;
+					})
+					if(a){
+						a.weight +=1;
+					}else if(a === undefined){
+						tagArray.push({text: item2, weight: 1, link: item2});
 					}
 				});
 			});
-		})();
-
-		main.collapse = function ($event,_id){
-			var col = angular.element($event.currentTarget),
-				panel = col.parent().find('#collapse_'+_id);
-			$scope.col[_id] = !panel.hasClass('in');
-		}
+			$scope.data = tagArray;
+		});
 		
-		main.play = function ($event){
+	};
+
+	main.$inject = ['$scope', '$http', '$state', '$stateParams', '$location', '$anchorScroll'];
+	app.controller('MainCtrl', main);
+
+// tags controller
+	function contentController($scope, $http, $state, $stateParams){
+		var content = this;
+		var tag = $stateParams.tags || null;
+		$scope.tag = tag || '';
+		$scope.sounds = [];
+		$scope.newSounds = [];
+		$scope.mob = [];
+		if(tag == null){
+			$http.get('sounds.json').then(function(res){
+				//$scope.sounds = res.data.files;
+				$scope.newSounds = res.data.files;
+				$scope.mob = res.data.files;
+			});
+		}else{
+			$http.get('sounds.json').then(function(res){
+				$scope.newSounds = res.data.files;
+				$scope.mob = res.data.files;
+				res.data.files.forEach(function(item,index){//files
+					var a = _.find(item.tag, function(tags){
+						return tags == tag;
+					});
+					if(a == tag){
+						$scope.sounds.push(item);
+					}
+				});
+
+			})
+		}
+
+		var befAudioId = null;
+
+		content.play = function ($event){
 			var audioWrap = angular.element($event.currentTarget),
 				id = audioWrap[0].id,
-				audioEle = audioWrap.find('audio'),
+				audioObj = audioWrap.find('audio'),
 				audio = audioWrap.find('audio')[0],
 				progress = audioWrap.find('.progress'),
 				icon = audioWrap.find('.icon'),
 				befAudio = angular.element(document).find('audio.'+befAudioId)[0] || null;
-
 			if(befAudio !== null){
 				if(befAudioId == id){
 					if (audio && audio.readyState >= 1) audio.paused ? audio.play() : audio.pause();
 				}else{
-					var befAudioEle = angular.element(befAudio);
+					var befAudioObj = angular.element(befAudio);
 					befAudio.pause();
 					befAudio.currentTime = 0;
-					befAudioEle.siblings('.icon').removeClass('play pause');
-					befAudioEle.siblings('.progress').css('height', 0);
+					befAudioObj.siblings('.icon').removeClass('play pause');
+					befAudioObj.siblings('.progress').css('height', 0);
 					befAudioId = id;
 					if (audio && audio.readyState >= 1) audio.paused ? audio.play() : audio.pause();
 				}
@@ -59,11 +121,11 @@
 				if (audio && audio.readyState >= 1) audio.paused ? audio.play() : audio.pause();
 			}
 			
-			audioEle.bind('play', function (){
+			audioObj.bind('play', function (){
 				icon.removeClass('pause').addClass('play');
 				progress.animate({height: 100+'%'}, (audio.duration - audio.currentTime)*1000, 'linear');
 			});
-			audioEle.bind('pause', function (){
+			audioObj.bind('pause', function (){
 				progress.stop();
 				if(audio.currentTime == 0){
 					progress.css('height', 0);
@@ -73,41 +135,16 @@
 					icon.removeClass('play').addClass('pause');
 				}
 			});
-			audioEle.bind('ended', function (){
+			audioObj.bind('ended', function (){
 				progress.stop();
 				progress.css('height', 0);
 				icon.removeClass('play pause');
 				audio.load();
 			});
 		};
-	}]);
+	};
 
-	app.directive('coll',[function(){
-		return{
-			restrict: 'A',
-			scope:{
-				sound: '=sound',
-				col: '=cl',
-				main: '=main'
-			},
-			link: function (scope, ele, attrs){
-				var index = scope.$parent.$index;
+	contentController.$inject = ['$scope', '$http', '$state', '$stateParams'];
+	app.controller('contentCtrl', contentController);
 
-				if (scope.$parent.$index == 0){
-					angular.element(ele).addClass('in');
-				}
-				
-				scope.$watch('col['+index+']', function(){
-					if(scope.col[index]){
-						scope.contentUrl = 'view/col.html';
-						scope.$parent.isLoaded[index] = true;
-					}else if(!scope.col[index] && !scope.$parent.isLoaded[index]){
-						scope.contentUrl = 'view/default.html';
-					}
-				})
-			},
-			template: '<div ng-include="contentUrl"></div>'
-		}
-	}]);
 })();
-
